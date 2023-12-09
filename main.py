eba3a14895d43707888b5f87138e71cf759ce1e9
Vyacheslav1557 <slavabirin1@yandex.ru>
2023-12-09 22:05:37 +0500

    refactor: исправление опечаток

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv('data.csv', delimiter=',', )
-
-df.columns = df.columns.str.lower().str.replace(' ', '_')
+from IPython.display import display
 
 
 def f(x):
     if x == 1.0:
         return x
-
-
-print(df.head(10))
 
 
 # Напишем функцию для первичной проверки данных
@@ -25,26 +19,23 @@ def check_data(data_df):
     missed_cells = data_df.isnull().sum().sum() / (data_df.shape[0] * (data_df.shape[1] - 1))
     missed_rows = sum(data_df.isnull().sum(axis=1) > 0) / data_df.shape[0]
 
-    print('\033[1m' + '\пПроверка пропусков' + '\033[0m')
+    print('\033[1m' + '\nПроверка пропусков' + '\033[0m')
     print('Количество пропусков: {:.0f}'.format(data_df.isnull().sum().sum()))
-    print('Доля пропусков: (:.1%)'.format(missed_cells) + '\033[0m')
-    print('Доля строк содержащих пропуски: (:.1%)'.format(missed_rows))
-
-    ### Проверим дубликаты
+    print('Доля пропусков: {:.1%}'.format(missed_cells) + '\033[0m')
+    print('Доля строк содержащих пропуски: {:.1%}'.format(missed_rows))
 
     print('\033[1m' + '\nПроверка на дубликаты' + '\033[0m')
 
     print('Количество полных дубликатов: ', data_df.duplicated().sum())
 
-    ## Посмотрим на сами данные
     print('\033[1m' + "\nПервые пять строк датасета" + '\033[0m')
-    display(data_df.head())
+    display(data_df.head(10))
 
-    print("\033[1m" + '\nОпописание количественных данных:' + '\033[em')
+    print("\033[1m" + '\nОпописание количественных данных:' + '\033[0m')
 
     display(data_df.describe().T)
 
-    print("\033[1m" + '\nОписание категориальных данных: ' + '\033[em')
+    print("\033[1m" + '\nОписание категориальных данных: ' + '\033[0m')
 
     display(data_df.describe(include='object').T)
 
@@ -53,5 +44,12 @@ def check_data(data_df):
     df_object = data_df.select_dtypes(include='object').columns
 
     for i in df_object:
-        print("\033[1m" + "_" + str(1) + '\033[em')
+        print("\033[1m" + "_" + str(i) + '\033[0m')
         display(data_df[i].value_counts())
+
+
+if __name__ == "__main__":
+    df = pd.read_csv('data.csv', delimiter=',', )
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+    check_data(df)
+    print(df.head(10))
